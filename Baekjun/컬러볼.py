a40aca4df818f28f23332b097df498f79a70712c
Coaspe <aspalt85@gmail.com>
2023-05-01 12:36:58 +0900

    Update 컬러볼.py

--- Baekjun/컬러볼.py
+++ Baekjun/컬러볼.py
@@ -12,14 +12,14 @@ if __name__ == "__main__":
 
     answer = defaultdict(int)
     ball_size_sum = defaultdict(int)
-
     total = 0
-    i, j = 0, 0
+    j = 0
     for i in range(n):
         while ball[j][1] < ball[i][1]:
             total += ball[j][1]
             ball_size_sum[ball[j][0]] += ball[j][1]
             j += 1
         answer[ball[i][2]] = total - ball_size_sum[ball[i][0]]
+
     for i in range(n):
         print(answer[i])
