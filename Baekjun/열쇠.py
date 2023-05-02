dd24642877c830ef27e108b3e2be287bf580ede0
Coaspe <aspalt85@gmail.com>
2023-05-02 22:29:00 +0900

    Update 열쇠.py

--- Baekjun/열쇠.py
+++ Baekjun/열쇠.py
@@ -1,84 +1,70 @@
 from collections import deque
-T = int(input())
+import sys
+
+input = sys.stdin.readline
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
 
 
-def bfs(h, w):
-    q = deque([(h, w)])
-    check = set([(h, w)])
-
+def bfs(x, y):
+    q = deque()
+    c = [[0]*(w+2) for _ in range(h+2)]
+    q.append([x, y])
+    c[x][y] = 1
+    cnt = 0
     while q:
-        r, c = q.popleft()
+        x, y = q.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < h+2 and 0 <= ny < w+2:
+                if not c[nx][ny]:
+                    if a[nx][ny] == '.':
+                        c[nx][ny] = 1
+                        q.append([nx, ny])
+                    elif a[nx][ny].islower():
+                        door[ord(a[nx][ny]) - ord('a')] = 1
+                        q = deque()
+                        c = [[0]*(w+2) for _ in range(h+2)]
+                        a[nx][ny] = '.'
+                        q.append([nx, ny])
+                    elif a[nx][ny].isupper():
+                        if door[ord(a[nx][ny]) - ord('A')]:
+                            c[nx][ny] = 1
+                            a[nx][ny] = '.'
+                            q.append([nx, ny])
+                    elif a[nx][ny] == '$':
+                        c[nx][ny] = 1
+                        cnt += 1
+                        a[nx][ny] = '.'
+                        q.append([nx, ny])
+    print(cnt)
 
-        for row, col in (r+1, c), (r-1, c), (r, c+1), (r, c-1):
-            if 0 <= row < H and 0 <= col < W and (row, col) not in check and M[row][col] != '*':
-                charToint = ord(M[row][col])
-                global tt
-                if M[row][col] == '$':
-                    global ans
-                    ans += 1
-                # Upper case
-                elif 65 <= charToint <= 90:
-                    if M[row][col].lower() not in keys:
-                        continue
-                    tt += 1
-                # Lower case
-                elif 97 <= charToint <= 122:
-                    keys.add(M[row][col])
-                    tt += 1
 
-                M[row][col] = '.'
-                check.add((row, col))
-                q.append((row, col))
+def new_map():
+    for i in a:
+        i.insert(0, '.')
+        i.append('.')
+    a.insert(0, ['.']*(w+2))
+    a.append(['.']*(w+2))
 
 
-while T:
+tc = int(input())
+while tc:
+    h, w = map(int, input().split())
+    a = [list(input().strip()) for _ in range(h)]
+    key = list(input().strip())
+    door = [0]*26
 
-    T -= 1
+    for k in key:
+        if k != '0':
+            door[ord(k) - ord('a')] = 1
 
-    H, W = map(int, input().split())
-    M = [list(input()) for _ in range(H)]
-    keys = set(list(input()))
-    start_points = []
-    start_candid = set()
-
-    for h in range(H):
-        for w in range(W):
-            if M[h][w] != '*' and (h == H - 1 or w == W - 1 or h == 0 or w == 0):
-                charToint = ord(M[h][w])
-                if 97 <= charToint <= 122:
-                    keys.add(M[h][w])
-                    M[h][w] = '.'
-                    start_points.append((h, w))
-                elif 65 <= charToint <= 90:
-                    start_candid.add((h, w))
-                else:
-                    start_points.append((h, w))
-
-    for h, w in start_candid:
-        if M[h][w].lower() in keys:
-            M[h][w] = '.'
-            start_points.append((h, w))
-            start_candid.remove((h, w))
-
-    ans = 0
-
-    while True:
-        tmp = ans
-        tt = 1
-        while tt:
-            tt -= 1
-            for h, w in start_points:
-                bfs(h, w)
-            removed = []
-            for h, w in start_candid:
-                if M[h][w].lower() in keys:
-                    start_points.append((h, w))
-                    removed.append((h, w))
-                    M[h][w] = '.'
-
-            for h, w in removed:
-                start_candid.remove((h, w))
-        if tmp == ans:
-            break
-
-    print(ans)
+    for i in range(h):
+        for j in range(w):
+            if ord('A') <= ord(a[i][j]) <= ord('Z'):
+                if door[ord(a[i][j]) - ord('A')]:
+                    a[i][j] = '.'
+    new_map()
+    bfs(0, 0)
+    tc -= 1
