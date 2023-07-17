969b692292f9aa7bf4db78ef38df95884bd5ceb2
Coaspe <aspalt85@gmail.com>
2023-07-18 07:38:24 +0900

    Solve 환승

--- Baekjun/7.2/세 용액/solution.py
+++ Baekjun/7.2/세 용액/solution.py
@@ -1,24 +1,25 @@
-import sys
+# 세 용액
 
 n = int(input())
-array = list(map(int, input().split()))
+arr = list(map(int, input().split()))
+arr.sort()
 
-array.sort()
-minTake = sys.maxsize
-
-for i in range(n-2):
-    start = i + 1
-    end = n - 1
-    while start < end:
-        take = array[i] + array[start] + array[end]
-        if abs(take) < minTake:
-            minTake = abs(take)
-            result = [array[i], array[start], array[end]]
-        if take < 0:
-            start += 1
-        elif take > 0:
-            end -= 1
+cand = [10**10, 0, 0, 0]
+for i in range(0, n - 2):
+    l, r = i + 1, n - 1
+    while (l < r):
+        sum = arr[i] + arr[l] + arr[r]
+        if abs(sum) < cand[0]:
+            cand[0] = abs(sum)
+            cand[1] = arr[i]
+            cand[2] = arr[l]
+            cand[3] = arr[r]
+        if sum > 0:
+            r -= 1
+        elif sum < 0:
+            l += 1
         else:
-            break
+            print(cand[1], cand[2], cand[3])
+            exit(0)
 
-print(result[0], result[1], result[2])
+print(cand[1], cand[2], cand[3])
