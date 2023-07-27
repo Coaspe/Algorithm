a69a94b0f196a31fd62b5a790fd025353041bc69
Coaspe <aspalt85@gmail.com>
2023-07-27 16:28:10 +0900

    Solve 볼록 껍질

---
+++ Algorithms Implementations/Convex Hull/graham.py
@@ -0,0 +1,27 @@
+def cross(p1, p2, p3):
+    return (p2[0] - p1[0])*(p3[1] - p1[1]) - (p2[1] - p1[1])*(p3[0] - p1[0])
+
+
+N = int(input())
+
+
+def slope(p1, p2):
+    return 1.0*(p1[1]-p2[1])/(p1[0]-p2[0]) if p1[0] != p2[0] else float('inf')
+
+
+points = [tuple(map(int, input().split())) for _ in range(N)]
+
+start = min(points, key=lambda p: (p[0], p[1]))
+
+points.pop(points.index(start))
+
+points.sort(key=lambda p: (slope(p, start)))
+
+ans = [start]
+
+for p in points:
+    while len(ans) >= 2 and (cross(ans[-2], ans[-1], p) <= 0):
+        ans.pop()
+    ans.append(p)
+
+print(len(ans))
