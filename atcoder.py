f31700f52e0b400c843cb35d8d9fc22158abcd8e
Coaspe <aspalt85@gmail.com>
2023-08-01 02:28:07 +0900

    Update atcoder.py

--- atcoder.py
+++ atcoder.py
@@ -1,24 +1,40 @@
-N = 10
-P = [i for i in range(N)]
-rank = [0]*N
+from collections import deque
+d = deque()
+N, M = map(int, input().split())
+A = []
+for i in range(N):
+    s = input()
+    A.append(s)
 
+V = [[[0]*5 for _ in range(M)] for _ in range(N)]
 
-def find(x):
+d.append((1, 1, 4))
 
-    if P[x] != x:
-        P[x] = find(P[x])
+V[1][1][4] = 1
 
-    return P[x]
+E = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
+while d:
+    y, x, dd = d.popleft()
+    if dd != 4:
+        ny, nx = y+E[dd][0], x+E[dd][1]
+        if A[ny][nx] == '.' and V[ny][nx][dd] == 0:
+            V[ny][nx][dd] = 1
+            d.append((ny, nx, dd))
+        elif A[ny][nx] == '#' and V[y][x][4] == 0:
+            V[y][x][4] = 1
+            d.append((y, x, 4))
 
-def union(x, y):
-    px = find(x)
-    py = find(y)
-
-    if rank[px] > rank[py]:
-        P[py] = px
-    elif rank[py] > rank[px]:
-        P[px] = py
     else:
-        P[py] = px
-        rank[px] += 1
+        for i in range(4):
+            dy, dx = E[i]
+            ny, nx, dd = y+dy, x+dx, i
+            if V[ny][nx][i] == 0 and A[ny][nx] == '.':
+                V[ny][nx][i] = 1
+                d.append((ny, nx, i))
+
+ans = 0
+for i in range(N):
+    for j in range(M):
+        ans += max(V[i][j])
+print(ans)
