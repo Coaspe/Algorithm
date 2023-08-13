554cd5736df1be27e61785b6b650d55fcb1b9ebb
Coaspe <aspalt85@gmail.com>
2023-08-13 21:39:41 +0900

    Solve Baekjun/1865. 웜홀

---
+++ Baekjun/8.2/1865. 웜홀/solution.py
@@ -0,0 +1,40 @@
+from sys import maxsize
+
+T = int(input())
+
+
+def bf():
+    graph = []
+    dist = [maxsize] * (N + 1)
+    dist[1] = 0
+
+    for _ in range(M):
+        a, b, c = map(int, input().split())
+        graph.append((a, b, c))
+        graph.append((b, a, c))
+
+    for _ in range(W):
+        a, b, c = map(int, input().split())
+        graph.append((a, b, -c))
+
+    for i in range(N):
+        for s, e, c in graph:
+            if dist[s] != maxsize and dist[e] > dist[s] + c:
+                dist[e] = dist[s] + c
+
+                if i == N - 1:
+                    print("YES")
+                    return
+
+    for i in dist:
+        if i < 0:
+            print("YES")
+            return
+    print("NO")
+
+
+while T:
+    T -= 1
+    N, M, W = map(int, input().split())
+
+    bf()
