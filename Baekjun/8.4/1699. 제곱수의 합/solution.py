fa6a93abda0dcc7089a5195e00f559fdf1b6ba77
coaspe <aspalt85@gmail.com>
2023-08-23 14:16:23 +0900

    Solve Baekjun/1699. 제곱수의 합
    Classification: DP

---
+++ Baekjun/8.4/1699. 제곱수의 합/solution.py
@@ -0,0 +1,13 @@
+N = int(input())
+
+dp = [i for i in range(N + 1)]
+
+for i in range(1, N + 1):
+    for j in range(1, i):
+        if j * j > i:
+            break
+        if dp[i] > dp[i - j * j] + 1:
+            dp[i] = dp[i - j * j] + 1
+
+
+print(dp[-1])
