feb64fb32bcc12f41fac6b34ee740b2075376d5a
coaspe <aspalt85@gmail.com>
2023-09-07 17:15:18 +0900

    Solve LeetCode/1272. Remove Interval
    Classification: Array

---
+++ LeetCode/1272. Remove Interval/solution.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+class Solution:
+    def removeInterval(
+        self, intervals: List[List[int]], toBeRemoved: List[int]
+    ) -> List[List[int]]:
+        remove_start, remove_end = toBeRemoved
+        output = []
+
+        for start, end in intervals:
+            # If there are no overlaps, add the interval to the list as is.
+            if start > remove_end or end < remove_start:
+                output.append([start, end])
+            else:
+                # Is there a left interval we need to keep?
+                if start < remove_start:
+                    output.append([start, remove_start])
+                # Is there a right interval we need to keep?
+                if end > remove_end:
+                    output.append([remove_end, end])
+
+        return output
