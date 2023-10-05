5ddd2e0415e287d1f37c28b6c50907cf588517be
coaspe <aspalt85@gmail.com>
2023-10-05 10:30:20 +0900

    Solve Baekjun/21757. 나누기
    Classification: DP

---
+++ Baekjun/10.1/21757. 나누기/solution.py
@@ -0,0 +1,18 @@
+N = int(input())
+arr = list(map(int, input().split()))
+
+total = 0
+sa = []
+for i in arr:
+    total += i
+    sa.append(total)
+if total % 4 != 0:
+    print(0)
+else:
+    dp = [1, 0, 0, 0]
+    quater = total // 4
+    for i in range(N - 1):
+        for j in range(3, 0, -1):
+            if sa[i] == quater * j:
+                dp[j] += dp[j - 1]
+        print(dp, i, sa[i])
