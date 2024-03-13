6ff2359a70102417ac81576b44f73bf989ff90af
coaspe <aspalt85@gmail.com>
2024-03-13 17:13:07 +0900

    Solve Baekjun/2065. 나룻배

--- Baekjun/24.3.2/2065. 나룻배/solution.py
+++ Baekjun/24.3.2/2065. 나룻배/solution.py
@@ -1,42 +1,47 @@
 from collections import deque
 import sys
 
-input = lambda: sys.stdin.readline().rstrip()
-ints = lambda: map(int, input().split())
+input = sys.stdin.readline
 
-M, t, N = ints()
+M, t, N = map(int, input().split())
 a = []
 for i in range(N):
     arrival, dest = input().split()
-    a.append((int(arrival), int(dest == "right"), i))
+    a.append((int(arrival), int(dest == "left"), i))
 a.sort()
 
 time = 0
-next_passengers = deque(a)
-waiting_passengers = [deque(), deque()]
-riding_passengers = deque()
-ship_location = 0
+next = deque(a)
+waiting = [deque(), deque()]
+riding = deque()
+loc = 1
 
 res = [0] * N
-while next_passengers or waiting_passengers[0] or waiting_passengers[1]:
-    if waiting_passengers[ship_location]:
+while next or waiting[0] or waiting[1]:
+
+    # 현재 방향 기준으로 기다리는 승객이 있는지 확인
+    if waiting[loc]:
         pass
-    elif waiting_passengers[ship_location ^ 1]:
+    elif waiting[loc ^ 1]:
         time += t
-        ship_location ^= 1
+        loc ^= 1
     else:
-        time = max(time, next_passengers[0][0])
+        time = max(time, next[0][0])
 
-    while next_passengers and next_passengers[0][0] <= time:
-        waiting_passengers[next_passengers[0][1]].append(next_passengers.popleft())
+    # 현재 시각까지 도착한 승객을 waiting 큐에 삽입
+    while next and next[0][0] <= time:
+        waiting[next[0][1]].append(next.popleft())
 
-    while waiting_passengers[ship_location] and len(riding_passengers) < M:
-        riding_passengers.append(waiting_passengers[ship_location].popleft())
+    # 현재 위치에 있는 승객들 중 태울 수 있는 손님들 태움
+    while waiting[loc] and len(riding) < M:
+        riding.append(waiting[loc].popleft())
 
+    # 반대편으로 이동
     time += t
-    ship_location ^= 1
+    loc ^= 1
 
-    while riding_passengers:
-        res[riding_passengers.popleft()[2]] = time
+    # 반대편에서 내림
+    while riding:
+        res[riding.popleft()[2]] = time
 
 print("\n".join(map(str, res)))
