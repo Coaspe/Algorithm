71640c50ee96f4fd2ce78bcf1914812c49a177f4
Coaspe <aspalt85@gmail.com>
2025-09-11 21:08:12 +0900

    Update solution.py

--- Baekjun/24.9.1/32178. 용액 2/solution.py
+++ Baekjun/24.9.1/32178. 용액 2/solution.py
@@ -1,39 +1,28 @@
-from sys import maxsize
-
-
-def find_closest_to_zero(N, s):
-    prefix_sum = [0] * (N + 1)
-    for i in range(1, N + 1):
-        prefix_sum[i] = prefix_sum[i - 1] + s[i - 1]
-
-    indexed_prefix_sum = [(prefix_sum[i], i) for i in range(N + 1)]
-
-    indexed_prefix_sum.sort()
-
-    min_diff = maxsize
-    ans_diff = None
-    best_l, best_r = 0, 0
-
-    for i in range(1, N + 1):
-        diff = abs(indexed_prefix_sum[i][0] - indexed_prefix_sum[i - 1][0])
-        if diff < min_diff:
-            min_diff = diff
-            l = indexed_prefix_sum[i - 1][1]
-            r = indexed_prefix_sum[i][1]
-            if l > r:
-                ans_diff = indexed_prefix_sum[i - 1][0] - indexed_prefix_sum[i][0]
-            else:
-                ans_diff = indexed_prefix_sum[i][0] - indexed_prefix_sum[i - 1][0]
-            best_l = min(l, r) + 1
-            best_r = max(l, r)
-
-    return ans_diff, best_l, best_r
-
+from bisect import bisect_left
 
 N = int(input())
-s = list(map(int, input().split()))
+A = [int(input()) for _ in range(N)]
 
-min_diff, L, R = find_closest_to_zero(N, s)
+ans = 0
 
-print(min_diff)
-print(L, R)
+
+def insert(arr, val):
+    idx = bisect_left(arr, val)
+    if idx == len(arr):
+        arr.append(val)
+    else:
+        arr[idx] = val
+
+
+for i in range(N):
+    l = [A[i]]
+    d = [-A[i]]
+
+    for j in range(i + 1, N):
+        if A[j] > A[i]:
+            insert(l, A[j])
+        else:
+            insert(d, -A[j])
+
+    ans = max(ans, len(d) + len(l) - 1)
+print(ans)
